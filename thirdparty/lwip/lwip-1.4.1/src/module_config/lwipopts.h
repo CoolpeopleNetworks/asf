ecc892d4a9bdacb7b1650cfab6c58cb998e95282
John W. Terrell <john.terrell@outlook.com>
2019-12-05 09:05:07 -0800

    WIP

--- thirdparty/lwip/lwip-1.4.1/src/module_config/lwipopts.h
+++ thirdparty/lwip/lwip-1.4.1/src/module_config/lwipopts.h
@@ -3,4 +3,13 @@
 
 #include "conf_lwip_threads.h"
 
+#define LWIP_AUTOIP                 1
+#define LWIP_DHCP_AUTOIP_COOP       1
+#define LWIP_DHCP_AUTOIP_COOP_TRIES 4
+#define LWIP_IGMP                   1
+#define LWIP_RAND() rand()
+#define LWIP_DNS                    1
+#define LWIP_SO_RCVTIMEO            1
+#define LWIP_SO_SNDTIMEO            1
+
 #endif
