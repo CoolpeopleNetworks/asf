7314282221ab1aa2666b032b4a4cdb98c0e4ee19
John W. Terrell <john@coolpeoplenetworks.com>
2019-12-10 16:23:29 +0000

    WIP

--- sam/drivers/pmc/sleep.c
+++ sam/drivers/pmc/sleep.c
@@ -41,12 +41,15 @@
 #if (SAM3S || SAM3N || SAM3XA || SAM3U || SAM4S || SAM4E || SAM4N || SAM4C || \
 		SAM4CM || SAMG || SAM4CP || SAMV71 || SAMV70 || SAMS70 || SAME70)
 # include "pmc.h"
+
+#ifdef COOLPEOPLE
 # include "board.h"
 
 /* Checking board configuration of main clock xtal statup time */
 #if !defined(BOARD_OSC_STARTUP_US)
 # warning The board main clock xtal statup time has not been defined. Using default settings.
 # define BOARD_OSC_STARTUP_US    (15625UL)
+#endif
 #endif
 
 #if !defined(EFC0)
