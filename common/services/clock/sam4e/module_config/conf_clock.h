dd4756c8e2ca236f7c8396954dc7de0220377b32
John W. Terrell <john.terrell@outlook.com>
2019-12-26 09:21:18 -0800

    WIP

--- common/services/clock/sam4e/module_config/conf_clock.h
+++ common/services/clock/sam4e/module_config/conf_clock.h
@@ -49,8 +49,8 @@
 #define CONFIG_SYSCLK_SOURCE        SYSCLK_SRC_PLLACK
 
 // ===== System Clock (MCK) Prescaler Options   (Fmck = Fsys / (SYSCLK_PRES))
-#define CONFIG_SYSCLK_PRES          SYSCLK_PRES_1
-//#define CONFIG_SYSCLK_PRES          SYSCLK_PRES_2
+//#define CONFIG_SYSCLK_PRES          SYSCLK_PRES_1
+#define CONFIG_SYSCLK_PRES          SYSCLK_PRES_2
 //#define CONFIG_SYSCLK_PRES          SYSCLK_PRES_4
 //#define CONFIG_SYSCLK_PRES          SYSCLK_PRES_8
 //#define CONFIG_SYSCLK_PRES          SYSCLK_PRES_16
@@ -60,15 +60,15 @@
 
 // ===== PLL0 (A) Options   (Fpll = (Fclk * PLL_mul) / PLL_div)
 // Use mul and div effective values here.
-#define CONFIG_PLL0_SOURCE          PLL_SRC_MAINCK_12M_RC
-#define CONFIG_PLL0_MUL             10
+#define CONFIG_PLL0_SOURCE          PLL_SRC_MAINCK_XTAL
+#define CONFIG_PLL0_MUL             16
 #define CONFIG_PLL0_DIV             1
 
 
 // ===== USB Clock Source Options   (Fusb = FpllX / USB_div)
 // Use div effective value here.
 #define CONFIG_USBCLK_SOURCE        USBCLK_SRC_PLL0
-#define CONFIG_USBCLK_DIV           10
+#define CONFIG_USBCLK_DIV           4
 
 // ===== Target frequency (System clock)
 // - XTAL frequency: 12MHz
